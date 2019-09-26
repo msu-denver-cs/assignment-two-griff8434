12032b3967fb45f05d198fd1b619bbffbc71bd2a
griff8434 <Umiami84@>
2019-09-26 11:37:20 -0600

    update

--- Carbuilder/app/controllers/cars_controller.rb
+++ Carbuilder/app/controllers/cars_controller.rb
@@ -73,6 +73,6 @@ class CarsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def car_params
-      params.require(:car).permit(:model, :VIN, :part_ids => [], :make_ids => [])
+      params.require(:car).permit(:model, :VIN, :part_ids => [], :make_id)
     end
 end
